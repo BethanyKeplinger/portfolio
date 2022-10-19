1a9022d62cc627c295f86012eab690d30c9b3e32
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-10-19 11:20:35 -0400

    Create Contact.js

---
+++ src/components/Contact.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+export default function Contact() {
+    const [ name, setName ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ message, setMessage ] = useState("");
+
+    function encode(data) {
+        return Object.keys(data).map(
+            (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+        )
+        .join("&")
+    }
+
+    function handleSubmit(e) {
+        e.preventdefault();
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", name, email, message }),
+        })
+            .then(() => alert("Message sent!"))
+            .catch((error) => alert(error));
+    }
+
+    return (
+        <section id="contact" className="">
+            <div>
+                <form className="contact form" onSubmit={handleSubmit}>
+
+                <h2>Get In Touch</h2>
+
+                <div className="myInfo">
+                    <h3>My Information</h3>
+                    <p><a href="mailto:bethanymkeplinger@gmail.com" className="button">bethanymkeplinger@gmail.com</a></p>
+
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="text" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="name" aria-describedby="name" name="name" onChange={(e) => setName(e.target.value)}/>
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="email" name="email" onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="message" className="form-label">Message</label>
+                    <textarea type="message" className="form-control" id="message" name="message" onChange={(e) => setMessage(e.target.value)}/>
+                </div>
+               
+                <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </section>
+    )
+    
+}
