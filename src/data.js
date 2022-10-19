a86969c9e8694ec242b349c9ce6cc20973c5a4a5
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-10-19 11:21:06 -0400

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,58 @@
+import photo1 from "./img/profile-view.png";
+import photo2 from "./img/react-movie-view.png";
+import photo3 from "./img/MeetApp.png";
+import photo4 from "./img/chatapp.png";
+import photo5 from "./img/charmander.png"
+
+export const projects = [
+    {
+        title: "myFlix [Angular]",
+        subtitle: "MEAN Stack",
+        description: "MyFlix was built using Angular, TypeScript and Angular Materials.",
+        image: photo1,
+        linkdemo: "https://bethanykeplinger.github.io/myFlix-Angular-client/welcome",
+        linkcode: "https://github.com/BethanyKeplinger/myFlix-Angular-client",
+        login: "Username: testing1234 Password: testing1234"
+
+    },
+
+    {
+        title: "myFlix [React]",
+        subtitle: "MERN Stack",
+        description: "The client-side of this app was created using React, ReactDOM, Bootstrap, Axios, Bootstrap, Parcel, React-Redux and Redux. The server-side was created with Express, Node.js and MongodDB.",
+        image: photo2,
+        linkdemo: "https://myflix2022.netlify.app/",
+        linkcode: "https://github.com/BethanyKeplinger/myFlix-client",
+        login: "Username: testing1234 Password: testing1234"
+    },
+    
+    {
+        title: "Meet App",
+        subtitle: "React",
+        description: "Meet App is a serverless (AWS lambda), progressive web application (PWA) built with React using a test-driven development technique.",
+        image: photo3,
+        linkdemo: "https://bethanykeplinger.github.io/meet",
+        linkcode: "https://github.com/BethanyKeplinger/meet",
+        login: ""
+    },
+
+    {
+        title: "ChitChat App",
+        subtitle: "React Native",
+        description: "Chat App was developed using React Native, Expo and GiftedChat to create a messaging application",
+        image: photo4,
+        linkdemo: "",
+        linkcode: "https://github.com/BethanyKeplinger/Chat-App",
+        login: ""
+    },
+
+    {
+        title: "Pokedex App",
+        subtitle: "JavaScript",
+        description: "Pokedex App utilizes JavaScript, Bootstrap HTML and CSS.",
+        image: photo5,
+        linkdemo: "https://bethanykeplinger.github.io/Pokedex-App/",
+        linkcode: "https://github.com/BethanyKeplinger/Pokedex-App",
+        login: ""
+    }
+]
