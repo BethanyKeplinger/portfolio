2bc27c4ee58ad2d721083fc4373a123489d9443d
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-10-19 11:20:42 -0400

    Create Projects.js

---
+++ src/components/Projects.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { projects } from "../data";
+// import { Container, Row } from "react-bootstrap";
+
+// import "./projects.css"
+
+export default function Projects() {
+    return (
+        <section className="projects" id="projects">
+            <div className="container text-center" >
+                <div className="heading">
+                    <h1>Projects I've built</h1>
+                    {/* <p>Description</p> */}
+                </div>
+
+                {/* <div className="project-contents"> */}
+                    <div className="projects">
+                        {projects.map((project) => (
+                            <div  className="row project-list" key={project.image}>
+                                <div className="col project-photo">
+                                    <a href={project.linkdemo} target="_blank" rel="noreferrer">
+                                        <img 
+                                        alt="gallery"
+                                        className="gallery"
+                                        src={project.image} />
+                                    </a>
+                                </div>
+
+                                <div className="col project-details">
+                                    <h1 className="project-title">{project.title}</h1>
+                                    <h2 className="project-subtitle">{project.subtitle}</h2>
+                                    <p className="project-description">{project.description}</p>
+                                    <p className="login-info">{project.login}</p>
+                                    <a href={project.linkcode} target="_blank" rel="noreferrer" className="btn code-button" role="button">See code</a>
+                                </div>
+                            </div>
+                             
+                        ))}
+                    </div>
+                {/* </div> */}
+            </div>
+        </section>
+
+    )
+    
+}
