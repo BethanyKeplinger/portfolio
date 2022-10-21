790b3cdfd92e89e5d1f3f72eedbf371ad5b6e435
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-10-21 12:20:01 -0400

    Create CaseStudy1.js

---
+++ src/components/CaseStudy1.js
@@ -0,0 +1,11 @@
+
+export default function CaseStudy1 () {
+    return (
+        <section id="casestudy1">
+            <div className="container">
+            <h1>Case Study 1</h1>
+            </div>
+        </section>
+        
+    )
+}
