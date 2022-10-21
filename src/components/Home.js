8e754c9b0ec727d8c52ad62489ef0456c3c4dd60
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-10-21 12:21:05 -0400

    Update Home.js
    import framer-motion. Add animation to h1 and p

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,37 @@
 import React from "react";
-// import Jumbotron from "react-bootstrap/Jumbotron";
+import { motion } from "framer-motion"
 
 export default function Home () {
     return (
-        <section id="home" className="jumbotron">
+        <section id="home" className="jumbotron ">
             <div id="container home-container">
-                <h1 className="display-4 hero-title"> Hi, I'm Bethany Keplinger</h1>
-                <p className="lead">I'm a Software Engineer and nature super fan. </p>
+                <motion.div 
+                    animate={{
+                        x: '100px'    
+                    }}
+                    transition={{
+                        type: 'tween',
+                        duration: 2
+                    }}
+                    >
+                    <h1 className="display-4 hero-title"> Hi, I'm <span className="name-span">Bethany Keplinger,</span></h1>
+                    <p className="lead subtitle">I'm a Software Engineer. </p>
+                </motion.div>
 
                 <div className="button-container">
-                    <a href="#projects" className="btn btn-primary projects-button" role="button" >Take a Look <div className="fill"></div></a>
+                    <motion.a 
+                        animate={{backgroundColor: "#ae6bf2"}}
+                        transition= {{duration: "1", type: "tween"}}
+                        whileHover={{backgroundColor: "#3a6df0", 
+                            color: "white",                  
+                            boxShadow: "0px 0px 8px (rgb 255, 255, 255)",
+                        }}
+                        href="#projects" className="btn btn-lg projects-button" role="button" >Take a Look <div className="fill"></div>
+                    </motion.a>
                 </div>
 
-               
-                <hr className="my-4"/>
+                {/* btn btn-primary projects-button */}
+                {/* <hr className="my-4"/> */}
                 
             </div>
         </section>
