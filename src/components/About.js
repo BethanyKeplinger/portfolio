4df40b1603b312721dd650e0eb35fdcf2d658af4
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-10-21 12:19:57 -0400

    Update About.js
    Add bootstrap columns. Framer added to button.

--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,41 @@
 import React from "react";
-// import bethany from "./img/bethany.jpg"
+import { motion } from "framer-motion";
+import bethany from "../img/bethany.jpg";
+import resume from "../files/resume.pdf";
 
 export default function About() {
     return (
         <section id="about">
-            <div className="about-section">
-                <div>
-                    {/* <img src={bethany} alt="Headshot of Bethany Keplinger looking at the camera with plants on shelves behind her"></img> */}
-                </div>
-                <div>
-                    <p>Hello! I’m Bethany. I’m a web developer living in Portland, Maine seeking to support environmental
-                    sustainability in my work life. After several years in childhood
-                    development, I enrolled in a coding bootcamp and it turned out that programming required many similar
-                    skills, like problem
-                    solving and multi-tasking. As a developer, I want to use code to solve some of the planets’
-                    biggest problems.</p>
-
-                    <p>When I'm not working on my web development skills, you can find me in my garden, at a greenhouse
-                    or thrifting to find new treasures for my home.</p>
-
-                    <p>I am currently available for full-time work in the New England area or remotely.</p>
-
-                    {/* <p class="resume-link"> Download my Resume! <a href="files/Keplinger-Bethany-Resume.pdf" target="_blank"
-                    title="Download Resume"></a></p> */}
+            <div className="container about-section">
+                <div className="row">
+                    <div className="col-4">
+                        <img src={bethany} className="headshot" alt="Headshot of Bethany Keplinger looking at the camera with plants on shelves behind her"></img>
                     </div>
+                    <div className="col-8 about-contents">
+                        <p>Hello! I’m Bethany. I’m a web developer living in Portland, Maine seeking to support environmental
+                        sustainability in my work life. After several years in childhood
+                        development, I enrolled in a coding bootcamp and it turned out that programming required many similar
+                        skills, like problem
+                        solving and multi-tasking. As a developer, I want to use code to solve some of the planets’
+                        biggest problems.</p>
+
+                        <p>When I'm not working on my web development skills, you can find me in my garden, at a greenhouse
+                        or thrifting to find new treasures for my home.</p>
+
+                        <p>I am currently available for full-time work in the New England area or remotely.</p>
+
+                        {/* <div className="resumebtn-container"> */}
+                            <motion.a 
+                            animate={{backgroundColor: "#ae6bf2"}}
+                            transition= {{duration: "1", type: "tween"}}
+                            whileHover={{backgroundColor: "#3a6df0", 
+                                color: "white",                  
+                                boxShadow: "0px 0px 8px (rgb 255, 255, 255)",
+                            }}href= {resume} target="_blank" className="btn btn-lg resume-button" role="button" >Resume</motion.a>
+                        {/* </div> */}
+
+                    </div>
+                </div>    
             </div>
         </section>
     )
