c30041744bd713f8e2ba815d709fd9c58bb95d25
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-10-24 18:20:32 -0400

    Update Contact.js
    Update button to match projects and resume buttons.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 export default function Contact () {
@@ -7,7 +8,14 @@ export default function Contact () {
                 <h1 className="contact-title">Contact</h1>
                 <div className="contact-contents">
                     <h3 className="contact-sub">Interested in working together?</h3>
-                    <a href="mailto:bethanymkeplinger@gmail.com" className="btn btn-outline-dark contact-btn" role="button" rel="noopener noferrer">Yes!</a>
+                    <motion.a 
+                        transition= {{duration: ".8", type: "tween"}}
+                        whileHover={{backgroundColor: "#E2D1F9", 
+                            color: "#317773",                  
+                            boxShadow: "0px 0px 8px (rgb 255, 255, 255)",
+                        }}
+                        href="mailto:bethanymkeplinger@gmail.com" className="btn btn-lg projects-button" role="button" >Yes!
+                    </motion.a>
                 </div>
 
             </div>
