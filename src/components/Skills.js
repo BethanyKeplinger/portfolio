7388d34327e3a1eebd4ab998aa6186314cb778f5
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-10-24 13:29:10 -0400

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Skills() {
     return (
         <div id="skills">
-            
+            <p className="skills">HTML | CSS | JavaScript | React | Angular | Node.js | MongoDB</p>
         </div>
     )
     
