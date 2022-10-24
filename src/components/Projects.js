2fab6f030596317935b705bdaf8ca4159376e029
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-10-24 13:28:08 -0400

    Update Projects.js
    Swap project photo and description to opposite side

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,7 @@ import { projects } from "../data";
 export default function Projects() {
     return (
         <section className="projects" id="projects">
-            <div className="container text-center" >
+            <div className="container-fluid text-center" >
                 <div className="heading">
                     <h1>Projects</h1>
                     {/* <p>Description</p> */}
@@ -17,14 +17,6 @@ export default function Projects() {
                     <div className="projects">
                         {projects.map((project) => (
                             <div  className="row project-list" key={project.image}>
-                                <div className="col project-photo">
-                                    <a href={project.linkdemo} target="_blank" rel="noreferrer">
-                                        <img 
-                                        alt="gallery"
-                                        className="gallery"
-                                        src={project.image} />
-                                    </a>
-                                </div>
 
                                 <div className="col project-details">
                                     <h1 className="project-title">{project.title}</h1>
@@ -34,6 +26,16 @@ export default function Projects() {
                                     <a href={project.linkdemo} target="_blank" rel="noreferrer" className="btn code-button" role="button">Demo</a>
                                     <a href={project.linkcode} target="_blank" rel="noreferrer" className="btn code-button" role="button">Code</a>
                                 </div>
+                                <div className="col project-photo">
+                                    <a href={project.linkdemo} target="_blank" rel="noreferrer">
+                                        <img 
+                                        alt="gallery"
+                                        className="gallery"
+                                        src={project.image} />
+                                    </a>
+                                </div>
+
+                                
                             </div>
                              
                         ))}
