efec0b16f92c110efba4464070e999742faf5d21
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-10-24 18:19:42 -0400

    Update About.js
    Updated button animation.

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import bethany from "../img/bethany.jpg";
 import resume from "../files/resume.pdf";
 
+
 export default function About() {
     return (
         <section id="about">
@@ -12,7 +13,7 @@ export default function About() {
                         <img src={bethany} className="headshot" alt="Headshot of Bethany Keplinger looking at the camera with plants on shelves behind her"></img>
                     </div>
                     <div className="col-8 about-contents">
-                        <p>Hello! I’m Bethany. I’m a web developer living in Portland, Maine seeking to support environmental
+                        <p>I’m a web developer living in Portland, Maine seeking to support environmental
                         sustainability in my work life. After several years in childhood
                         development, I enrolled in a coding bootcamp and it turned out that programming required many similar
                         skills, like problem
@@ -23,13 +24,15 @@ export default function About() {
                         or thrifting to find new treasures for my home.</p>
 
                         <p>I am currently available for full-time work in the New England area or remotely.</p>
+                        
+                        <p>HTML | CSS | JavaScript | React | Angular | Node.js | MongoDB</p>
 
                         {/* <div className="resumebtn-container"> */}
                             <motion.a 
-                            animate={{backgroundColor: "#ae6bf2"}}
-                            transition= {{duration: "1", type: "tween"}}
-                            whileHover={{backgroundColor: "#3a6df0", 
-                                color: "white",                  
+                           
+                            transition= {{duration: ".8", type: "tween"}}
+                            whileHover={{backgroundColor: "#E2D1F9", 
+                                color: "#317773",                  
                                 boxShadow: "0px 0px 8px (rgb 255, 255, 255)",
                             }}href= {resume} target="_blank" className="btn btn-lg resume-button" role="button" >Resume</motion.a>
                         {/* </div> */}
