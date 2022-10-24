8a7c609773c22135961c71d05b27232553f94309
BethanyKeplinger <93795777+BethanyKeplinger@users.noreply.github.com>
2022-10-24 19:43:19 -0400

    Update Projects.js
    Add motion to gallery photos.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,22 +1,20 @@
 import React from "react";
 import { projects } from "../data";
-// import { Container, Row } from "react-bootstrap";
-
-// import "./projects.css"
+import { motion } from "framer-motion";
 
 export default function Projects() {
     return (
         <section className="projects" id="projects">
             <div className="container-fluid text-center" >
                 <div className="heading">
-                    <h1>Projects</h1>
+                    <h1 className="projects-title">Projects</h1>
                     {/* <p>Description</p> */}
                 </div>
 
                 {/* <div className="project-contents"> */}
-                    <div className="projects">
+                    <div className="project-main">
                         {projects.map((project) => (
-                            <div  className="row project-list" key={project.image}>
+                            <div  className="row align-items-center project-list" key={project.image}>
 
                                 <div className="col project-details">
                                     <h1 className="project-title">{project.title}</h1>
@@ -26,14 +24,15 @@ export default function Projects() {
                                     <a href={project.linkdemo} target="_blank" rel="noreferrer" className="btn code-button" role="button">Demo</a>
                                     <a href={project.linkcode} target="_blank" rel="noreferrer" className="btn code-button" role="button">Code</a>
                                 </div>
-                                <div className="col project-photo">
+                                <motion.div className="col project-photo" 
+                                    whileHover={{scale: 1.1}}>
                                     <a href={project.linkdemo} target="_blank" rel="noreferrer">
                                         <img 
                                         alt="gallery"
                                         className="gallery"
                                         src={project.image} />
                                     </a>
-                                </div>
+                                </motion.div>
 
                                 
                             </div>
